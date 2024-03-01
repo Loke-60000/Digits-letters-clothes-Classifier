6d1911a70b678776e2701f73f95b5eb51965c28f
Lokman <ramdani.lokman@gmail.com>
2024-03-01 11:59:41 +0100

    preprocessing added on api and testing

--- api/api.py
+++ api/api.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from tensorflow.keras.models import load_model
 from fastapi.middleware.cors import CORSMiddleware
-from PIL import Image
 import numpy as np
 import io
+import cv2
 
 app = FastAPI()
 
@@ -12,34 +12,42 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Load models
 modeldigits = load_model('../models/mnist_model_digits.h5')
 modelletters = load_model('../models/mnist_model_letters.h5')
 modelfashion = load_model('../models/mnist_model_fashion.h5')
 
-# Function to predict
 async def predict_model(file: UploadFile, model, response_type: str):
     if not file.filename.endswith(('.png', '.jpg', '.jpeg')):
         raise HTTPException(status_code=400, detail="Invalid file format.")
-    
-    image_data = await file.read()
-    image = Image.open(io.BytesIO(image_data)).convert('L')
-    image = image.resize((28, 28))
-    image_array = np.array(image)
-    image_array = image_array / 255.0
-    image_array = image_array.reshape(1, 28, 28, 1)
 
-    prediction = model.predict(image_array)
+    image_data = await file.read()
+    nparr = np.frombuffer(image_data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+    img = cv2.resize(img, (28, 28))
+
+    if img.shape[2] == 4:  
+        transparent_mask = img[:, :, 3] == 0
+        img[transparent_mask] = [255, 255, 255, 255]
+        img = img[:, :, :3]
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.equalizeHist(img_gray)
+    img_gray = img_gray / 255.0
+    img_gray = 1 - img_gray  
+    img_array = img_gray.reshape(1, 28, 28, 1)  
+
+    prediction = model.predict(img_array)
     if response_type == 'digit':
         result = np.argmax(prediction)
     elif response_type == 'letter':
         result = chr(65 + np.argmax(prediction))
     else:  # fashion
-        class_name = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+        class_name = ["T-shirt/top", "Trouser", "Pullover", "Dress",
+                      "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
         result = class_name[np.argmax(prediction)]
 
     return result
